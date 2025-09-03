e5a7a6b1870b7c4b5d6da9c9f7d3bdb790ba0928
brahman-himanshu <hd674552@gmail.com>
2025-09-03 15:13:18 +0530

    add the program

---
+++ python/list.py
@@ -0,0 +1,8 @@
+thislist=['hello','my name ', 'is', 'himanshu']
+print(thislist)
+print(thislist[1])  #access list item 
+thislist[1]="hey"  #change list item 
+print(thislist[1])
+#add item in list 
+thislist.append  ("sharma ")
+print(thislist)
