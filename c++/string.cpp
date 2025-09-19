8b907eebbc3977d01b9ca678c52f7669d38cc569
brahman-himanshu <hd674552@gmail.com>
2025-09-20 02:44:01 +0530

    c++

---
+++ c++/string.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main (){
+    string s= ("hello my age is 78 ");
+    cout<<s<<endl;
+    return 0 ;
+}
---
+++ c++/stringadd.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main (){
+    string s1 = ("himanshu");
+    string s2 = ( "  sharma  pandat ji hi kehde bhai ko bhi ");
+    string s3 ;
+    s3=s1+s2;
+    cout<<s3<<endl;
+    return 0 ;
+}
