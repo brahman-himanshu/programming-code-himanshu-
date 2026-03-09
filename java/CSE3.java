e3d9cec48bb67f6bee35d57e5f5da5471fee739b
brahman-himanshu <hd674552@gmail.com>
2026-03-09 17:49:34 +0530

    upload code

--- java/CSE3.java
+++
@@ -1,2 +0,0 @@
-class cse3{
-cse3()
---
+++ java/class1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Class1 
+{
+    public static void main(String a[])
+    {
+        Scanner sObj = new Scanner(System.in);
+        String name = sObj.nextLine();
+        System.out.println(name);
+        sObj.close();
+    }
+}
