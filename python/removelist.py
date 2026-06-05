3c7bd11306e181e98ff1cf7d90dc17fc4673b3ec
brahman-himanshu <hd674552@gmail.com>
2026-06-05 10:46:09 +0530

    removelist

--- python/removelist.py
+++ python/removelist.py
@@ -1,3 +1,6 @@
-thislist = ["apple", "banana", "cherry"]
-thislist.remove("banana")
-print(thislist)
+def remove_item(lst,item):
+  lst_remove(item)
+  return lst
+numer=[1,5,2,3,4,6]
+
+print(remove_item(numer,3))
