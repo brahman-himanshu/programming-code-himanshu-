96889dd9f989c04b546bc889816397adae9b64d2
brahman-himanshu <hd674552@gmail.com>
2026-07-20 23:12:04 +0530

    .

---
+++ DSA/DSA c++/recursion.cpp
@@ -0,0 +1,63 @@
+
+// n time name print 
+
+// #include<iostream>
+// using namespace std;
+// void fun(int i , int n ){
+//     if(i>n)
+//     return ;
+//     cout<< " kartik "<< " ";
+//     fun(i+1,n);
+// }
+// int main (){
+//     int n ;
+//     cin>>n;
+//     fun(1,n);
+// }
+
+
+
+// agar hame 1 to n tak recursion mein print karana ho toh ham fun ke pehle print karenge agar 
+// badme kara ya next line mein kara toh woh reverse mein ya n to 1 print hoga 
+
+
+// #include<iostream>
+// using namespace std ;
+// void fun(int i,int n){
+//     if (i>n)
+//     {
+//      return ;   /* code */
+//     }
+//     cout<<i<< " "; 
+//     fun(i+1,n);
+// }
+// int main(){
+//     int n ;
+// cin>>n;
+// fun(1,n);
+// }
+
+// n to 1 
+
+// #include<iostream>
+// using namespace std ;
+// void fun(int i,int n){
+//     if (i>n)
+//     {
+//      return ;   /* code */
+//     }
+    
+//     fun(i+1,n);
+//     cout<<i<< " "; 
+// }
+// int main(){
+//     int n ;
+// cin>>n;
+// fun(1,n);
+// }
+
+// sum n number 
+
+#include<iostream>
+using namespace std;
+
