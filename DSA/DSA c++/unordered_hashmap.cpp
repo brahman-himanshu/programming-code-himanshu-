ade6bfddcc4969fe980d8b97b6049eec4176e85e
brahman-himanshu <hd674552@gmail.com>
2026-07-25 22:21:52 +0530

    unordered hashmap

---
+++ DSA/DSA c++/unordered_hashmap.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <unordered_map>
+using namespace std ;
+int main (){
+    int n ;
+    cout<< "enter the value of n for enter the size of the array "<< endl;
+    cin >> n ;
+    int arr[n];
+    cout<<"enter the array "<<endl;
+    for (int i =0 ;i<n ; i++ ){
+        cin>> arr[i];
+    }
+  unordered_map<int , int > mpp ;
+  for (int i=0;i<n;i++){
+    mpp[arr[i]]++ ;
+  }
+
+
+  int q;
+  cout<<"enter the queries "<<endl;
+  cin >>q;
+  while(q--){
+    int number;
+    cout<<"enter the number "<<endl;
+    cin>>number;
+    cout<<mpp[number];
+  }
+    return 0 ;
+}
